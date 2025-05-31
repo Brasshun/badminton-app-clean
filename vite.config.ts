bbdd5e4125673e08d35b20b6b2c663af71598b6d
Brasshun <137479674+Brasshun@users.noreply.github.com>
2025-05-31 11:23:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/badminton-app-clean/',
@@ -10,16 +11,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(png|jpe?g|gif|svg|ico|webp)$/.test(assetInfo.name)) {
-            return `assets/images/[name][ext]`;
-          }
-          return `assets/[name][ext]`;
-        },
+        assetFileNames: 'assets/[name].[ext]',
         chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/index.js'
+        entryFileNames: 'assets/[name].js'
       }
     }
   }
